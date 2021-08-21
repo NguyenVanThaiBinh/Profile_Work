1354e7a99a5d84b9c123520d3bafc0243484a2bd
Binh <nguyenvanthaibinh2210@gmail.com>
2021-08-21 15:45:32 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
 // Animations
 AOS.init({
-  anchorPlacement: 'top-left',
-  duration: 1000
+    anchorPlacement: 'top-left',
+    duration: 1000
 });
 
 // Add your javascript here
+// Video 
+$(document).ready(function() {
+    $(".thevideo").on("mouseover", function(event) {
+        this.play();
+
+    }).on('mouseout', function(event) {
+        this.pause();
+
+    });
+})
