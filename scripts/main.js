b5dd0aafe5c94e572c22752f12a151ab78d3e409
Binh <nguyenvanthaibinh2210@gmail.com>
2021-08-25 21:44:22 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,27 @@
 // Animations
 AOS.init({
-    anchorPlacement: 'top-left',
-    duration: 1000
+    anchorPlacement: "top-left",
+    duration: 1000,
 });
 
 // Add your javascript here
-// Video 
+// Video
 $(document).ready(function() {
-    $(".thevideo").on("mouseover", function(event) {
-        this.play();
+    $(".thevideo")
+        .on("mouseover", function(event) {
+            this.play();
+        })
+        .on("mouseout", function(event) {
+            this.pause();
+        });
+});
 
-    }).on('mouseout', function(event) {
-        this.pause();
-
-    });
-})
+$(".avata_zoom").hover(
+    function() {
+        $("#ava_msg").css("display", "block");
+    },
+    function() {
+        // change to any color that was previously used.
+        $("#ava_msg").css("display", "none");
+    }
+);
