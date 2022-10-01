5a9461712f3a5035544c886e80337e1758d54728
Binh Hu <nguyenvanthaibinh2210@gmail.com>
2022-10-01 14:39:11 +0700

    update js image

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,27 @@
+// @ts-nocheck
 // Animations
 AOS.init({
-    anchorPlacement: "top-left",
-    duration: 1000,
+  anchorPlacement: "top-left",
+  duration: 1000,
 });
 
 // Add your javascript here
 // Video
-$(document).ready(function() {
-    $(".thevideo")
-        .on("mouseover", function(event) {
-            this.play();
-        })
-        .on("mouseout", function(event) {
-            this.pause();
-        });
+$(document).ready(function () {
+  $(".thevideo")
+    .on("mouseover", function (event) {
+      this.play();
+    })
+    .on("mouseout", function (event) {
+      this.pause();
+    });
 });
-
-$(".avata_zoom").hover(
-    function() {
-        $("#avata_cv").attr("title", "That better?");
-    },
-    function() {
-        // change to any color that was previously used.
-        $("#ava_msg").css("display", "none");
-    }
+$(".avatar_zoom").hover(
+  function () {
+    $("#avatar_cv").attr("title", "That better?");
+  },
+  function () {
+    // change to any color that was previously used.
+    $("#ava_msg").css("display", "none");
+  }
 );
